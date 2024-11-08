32f475404c052fac0f82de21ffe1c4c62589cf24
edmar.james <edmarjamesbautista@gmail.com>
2024-11-08 21:37:54 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var age = 30;
+var firstName = 'Edmar';
+var isFictional;
+// console.log(age);
+age = 21;
+firstName = 'James';
+isFictional = true;
+var planet = 'Earth';
+var moons = 1;
+var isLarge = false;
+planet = 'Mars';
+moons = 12;
+isLarge = true;
+// null & undefined
+var something;
+var anotherThing;
+something = null;
+anotherThing = undefined;
+console.log(typeof (age));
---
+++ index.ts
@@ -0,0 +1,24 @@
+let age: number = 30;
+let firstName: string = 'Edmar';
+let isFictional: boolean;
+
+// console.log(age);
+age = 21;
+firstName = 'James';
+isFictional = true;
+
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+
+planet = 'Mars';
+moons = 12;
+isLarge = true;
+
+// null & undefined
+let something: null;
+let anotherThing: undefined;
+
+something = null;
+anotherThing = undefined;
+
