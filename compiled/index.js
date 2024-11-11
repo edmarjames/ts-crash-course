a4026cf0389b17f3efd5c2c168e426fa13cf27bf
edmar.james <edmarjamesbautista@gmail.com>
2024-11-11 18:11:53 +0800

    Create a tsconfig file

--- compiled/index.js
+++ compiled/index.js
@@ -1,19 +1,19 @@
-var age = 30;
-var firstName = 'Edmar';
-var isFictional;
+"use strict";
+let age = 30;
+let firstName = 'Edmar';
+let isFictional;
 // console.log(age);
 age = 21;
 firstName = 'James';
 isFictional = true;
-var planet = 'Earth';
-var moons = 1;
-var isLarge = false;
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
 planet = 'Mars';
 moons = 12;
 isLarge = true;
 // null & undefined
-var something;
-var anotherThing;
+let something;
+let anotherThing;
 something = null;
 anotherThing = undefined;
-console.log(typeof (age));
